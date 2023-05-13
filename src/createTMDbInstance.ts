ff72f3ab0f68c19fa610bcc34ae351e5833b98ff
Dovydas Liniovas <41839786+LiniovasDovydas@users.noreply.github.com>
2023-05-13 08:29:30 +0300

    main instance return namespaces instead of spread methods

--- src/createTMDbInstance.ts
+++ src/createTMDbInstance.ts
@@ -6,21 +6,21 @@ import { reviewsRequestHandlers } from "./reviews";
 import { configurationRequestHandlers } from "./configuration";
 import { watchProvidersRequestHandlers } from "./watchProviders";
 
-export function createTMDbInstance(options: InstanceSchema = { apiKey: '' }) {
+export function createTMDbInstance(options: InstanceSchema = { apiKey: "" }) {
   const optionsResult = instanceSchema.safeParse(options);
 
   /** Validate passed options */
   if (optionsResult.success === false) {
-    printZodErrorGroup(optionsResult, 'TMDb instance options are invalid.');
+    printZodErrorGroup(optionsResult, "TMDb instance options are invalid.");
     return;
   }
 
   const { apiKey } = optionsResult.data;
 
   return {
-    ...configurationRequestHandlers(apiKey),
-    ...peopleRequestHandlers(apiKey),
-    ...reviewsRequestHandlers(apiKey),
-    ...watchProvidersRequestHandlers(apiKey),
+    config: configurationRequestHandlers(apiKey),
+    people: peopleRequestHandlers(apiKey),
+    reviews: reviewsRequestHandlers(apiKey),
+    watchProviders: watchProvidersRequestHandlers(apiKey),
   };
 }
